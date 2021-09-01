d6ece59b9cd1259ff016283dddf16999a89cbfdb
t00ns <67071434+t00ns@users.noreply.github.com>
2021-09-01 16:20:23 -0400

    Update clickndrag.js

--- clickndrag.js
+++ clickndrag.js
@@ -1,56 +1,14 @@
 const icon = document.querySelectorAll(".icon");
 const polaroid = document.querySelector(".polaroid");
 
-// icons click and drag //
-icon.addEventListener("mousedown", (e) => {
-  e.target.classList.add("moving");
-});
+// icons click and drag
 
-icon.addEventListener("mouseup", (e) => {
-  e.target.classList.add("moving");
-});
 
-addEventListener("mouseup", (e) => {
-  if (icon.classList.contains("moving")) {
-    icon.style.left = e.clientX + "px";
-    icon.style.top = e.clientY + "px";
-  }
-  icon.classList.remove("moving");
-});
+// polaroid click and drag
 
-addEventListener("mousemove", (e) => {
-  if (icon.classList.conatins("moving")) {
-    icon.style.left = e.clientX + "px";
-    icon.style.top = e.clientY + "px";
-  }
-});
 
-// polaroid click and drag //
 
-polaroid.addEventListener("mousedown", (e) => {
-  e.target.classList.add("moving");
-});
-
-polaroid.addEventListener("mouseup", (e) => {
-  e.target.classList.remove("moving");
-});
-
-addEventListener("mouseup", (e) => {
-  if (box.classList.contains("moving")) {
-    polaroid.style.left = e.clientX + "px";
-    polaroid.style.top = e.clientY + "px";
-  }
-  polaroid.classList.remove("moving");
-});
-
-addEventListener("mousemove", (e) => {
-  if (polaroid.classList.contains("moving")) {
-    polaroid.style.left = e.clientX + "px";
-    polaroid.style.top = e.clientY + "px";
-  }
-});
-
-// above code that is based on //
+// previous code that is based on
 box.addEventListener("mousedown", (e) => {
   e.target.classList.add("moving");
 });
